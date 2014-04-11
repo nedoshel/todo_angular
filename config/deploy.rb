b80702431759c21ca378646a9cd9d6ff8c6b934f
nedoshel <nedoshel@nedoshel-HP.(none)>
2014-04-11 13:35:57 +0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -101,13 +101,14 @@ end
 namespace :faye do
   desc "Start faye.ru server"
   task :start do
-    run "cd #{deploy_to}/current && bundle exec rackup faye.ru -E production -p 9293 -s thin --pid #{faye_pid} -D"
+    run "cd #{deploy_to}/current && bundle exec rake tmp:clear && bundle exec rackup faye.ru -E production -p 9293 -s thin --pid #{faye_pid} -D"
 
   end
 
   desc "Stop faye.ru server"
   task :stop do
-    run "cd #{deploy_to}/current;if [ -f #{faye_pid} ] && [ -e /proc/$(cat #{faye_pid}) ]; then kill -9 `cat #{faye_pid}`; fi"
+    #run "cd #{deploy_to}/current;if [ -f #{faye_pid} ] && [ -e /proc/$(cat #{faye_pid}) ]; then kill -9 `cat #{faye_pid}`; fi"
+    run "kill -9 `cat #{faye_pid}`"
   end
 
   desc "Restart faye.ru server"
--- faye.ru
+++ faye.ru
@@ -1,6 +1,6 @@
 # rackup sync.ru -E production
-require "bundler/setup"
-require "yaml"
+# require "bundler/setup"
+# require "yaml"
 require 'faye'
 
 Faye::WebSocket.load_adapter('thin')
@@ -10,11 +10,5 @@ app = Faye::RackAdapter.new(
   :mount => '/faye',
   :timeout => 25
 )
-
-
-#app.load_config(
-#  File.expand_path("../config/faye.yml", __FILE__),
-#  ENV["RAILS_ENV"] || "development"
-#)
 
 run app
